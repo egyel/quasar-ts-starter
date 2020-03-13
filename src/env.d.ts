2c3ad8da3f1d4b6046a68ef500d9be2ff4989a9a
tp <pepe1276@freemail.hu>
2020-03-13 19:45:02 +0100

    refactor(env): use shim (env.d.ts) for process.env

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    // Values supplied by Quasar CLI (https://quasar.dev/quasar-cli/cli-documentation/handling-process-env#Introduction)
+    DEV: boolean | undefined    // Code runs in development mode
+    PROD: boolean | undefined   // Code runs in production mode
+    CLIENT: boolean | undefined // Code runs on client (not on server)
+    SERVER: boolean | undefined // Code runs on server (not on client)
+    MODE: 'pwa' | 'ssr' | 'cordova' | 'capacitor' | 'electron' | undefined  // Quasar CLI mode (spa, pwa, …)
+    NODE_ENV: 'production' | 'development' | undefined  // Has two possible values: production or development
+
+    // Vue router (https://router.vuejs.org/guide/essentials/history-mode.html)
+    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined
+    VUE_ROUTER_BASE: string | undefined
+  }
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -22,7 +22,7 @@ export default function (/* { ssrContext } */) {
 
     // enable strict mode (adds overhead!)
     // for dev mode only
-    strict: process.env.DEV === 'true',
+    strict: process.env.DEV,
   });
 
   return Store;
