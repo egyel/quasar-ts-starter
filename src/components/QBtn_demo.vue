781c283864ee1597c857cdd08360d112821aabba
tp <pepe1276@freemail.hu>
2020-05-18 15:57:50 +0200

    test(QBtn_demo): try out test

---
+++ src/components/QBtn_demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p class="textContent">{{ input }}</p>
+    <span>{{ counter }}</span>
+    <q-btn id="mybutton" color="primary" icon="check" label="OK" @click="increment" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'QBUTTON',
+  data(): {
+    counter: number;
+    input: string;
+  } {
+    return {
+      counter: 0,
+      input: 'rocket muffin',
+    };
+  },
+  methods: {
+    increment(): void {
+      this.counter += 1;
+    },
+  },
+});
+</script>
